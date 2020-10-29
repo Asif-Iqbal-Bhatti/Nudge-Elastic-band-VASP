2b467ca89522fda566df77301b933d746ac0b5e4
Asif <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-10-29 16:48:54 +0100

    Update constraint_VASP_NEB.py

--- constraint_VASP_NEB.py
+++ constraint_VASP_NEB.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-#--------------------------------------------------------------------------------------
-AUTHOR :: Asif Iqbal
-#--------------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------------
+# AUTHOR :: Asif Iqbal
+# USAGE :: To constraint the atoms in the specific direction for DRAG calcualtions.
+# Copyright Asif Iqbal
+#-------------------------------------------------------------------------------------
 from ase import Atoms
 import ase.io
 from ase.io import write, read
@@ -11,30 +12,43 @@ from ase.constraints import FixAtoms, FixScaled, FixedPlane, FixedLine, FixCarte
 from ase.visualize.plot import plot_atoms
 import matplotlib.pyplot as plt
 
-#--------------------------------------------------------------------------------------
+
+def data_for_cylinder_along_z(center_x,center_y,radius,height_z):
+    z = np.linspace(0, height_z, 50)
+    theta = np.linspace(0, 2*np.pi, 50)
+    theta_grid, z_grid = np.meshgrid(theta, z)
+    x_grid = radius*np.cos(theta_grid) + center_x
+    y_grid = radius*np.sin(theta_grid) + center_y
+    return x_grid,y_grid,z_grid
+    
+c = []
 # Read initial and final states:
 initial = read_vasp('POSCAR')
 pos = initial.get_positions()
 ucell = initial.get_cell()
-atoms = Atoms(positions=pos, symbols='AxBxCxDxEx',cell=ucell, pbc=[True,True,True])
-#--------------------------------------------------------------------------------------
-c = []
+type = initial.get_chemical_symbols()
+atoms = Atoms(positions=pos, symbols=type, cell=ucell, pbc=[True,True,True])
+
 #c = FixAtoms(indices=[atom.index for atom in atoms if atom.symbol == 'Hf'])
 
-# """Constrain an atom index *a* to move in a given plane only.
-
-for atom in atoms:
-  c.append( FixedPlane( atom.index, ( 0, 0, 1) ) )
+#                """Constrain an atom index *a* to move in a given plane only.
+#for atom in atoms:
+#	c.append( FixedPlane(atom.index, ( 0, 0, 1)) )
+c = [ FixedPlane( atom.index, ( 0, 0, 1) )  for atom in atoms ]
 atoms.set_constraint(c)
 
 write_vasp("OUT.vasp", atoms=atoms, vasp5=True, ignore_constraints=False)
 #print (c)
-
-fig, ax = plt.subplots()
-ax.set(xlim=(-1, 1), ylim = (-1, 1))
-a_circle = plt.Circle((23, 11.5), 2.8, alpha = 0.5, capstyle='butt')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#ax.set_aspect("auto")
+ax.set(xlim=(-1, 1), ylim = (-1, 1) )
+a_circle = plt.Circle((23, 11.7), 2.7, alpha = 0.5,fill=False, ec='red')
+a1_circle = plt.Circle((23, 11.7), 0.2,fill=False, ec='red')
 ax.add_artist(a_circle)
-plot_atoms(atoms, ax, radii=0.5, rotation=('00x,00y,00z'))
+ax.add_artist(a1_circle)
+plot_atoms(atoms, ax, radii=0.5, rotation=('0x,0y,00z'))
+plt.savefig("test.eps", format="eps", dpi=600)
 plt.show()
 
 
